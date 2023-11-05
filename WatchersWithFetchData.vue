21a0ff3f2ca570069dc804545392b6165187d295
AnjasFedo <fedoaf98@gmail.com>
2023-11-05 17:51:49 +0700

    Create WatchersWithFetchData.vue

---
+++ WatchersWithFetchData.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, watch  } from 'vue'
+
+const todoId = ref(1)
+const todoData = ref(null)
+
+const newTodo = () => {
+  todoId.value++
+}
+
+async function fetchData() {
+  todoData.value = null
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+  )
+  todoData.value = await res.json()
+}
+
+fetchData()
+  
+watch(todoId, async(newTodo) => {
+  // yes, console.log() is a side effect
+  todoData.value = null
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+  )
+  todoData.value = await res.json()
+})
+</script>
+
+<template>
+  <p>Todo id: {{ todoId }}</p>
+  <button @click="newTodo">Fetch next todo</button>
+  <p v-if="!todoData">Loading...</p>
+  <pre v-else>{{ todoData }}</pre>
+</template>
