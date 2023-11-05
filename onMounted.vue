940257a1e4771c3eaf60071f9e5fd0e95f86e46a
AnjasFedo <fedoaf98@gmail.com>
2023-11-05 17:36:05 +0700

    Create onMounted.vue

---
+++ onMounted.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref, onMounted  } from 'vue'
+
+const pElementRef = ref(null)
+
+onMounted(() => {
+  pElementRef.value.textContent = 'anjas'
+})
+</script>
+
+<template>
+  <p ref="pElementRef">hello</p>
+</template>
