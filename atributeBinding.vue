a3580494e36017793ebee4fb8fe6d6066b8f093e
AnjasFedo <fedoaf98@gmail.com>
2023-11-05 19:04:49 +0700

    Create atributeBinding.vue

---
+++ atributeBinding.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const isRed = ref(true)
+const green = ref('green')
+
+const toggleGreen = () => {
+  green.value = green.value === 'green' ? 'blue' : 'green'
+}
+</script>
+
+<template>
+  <p :class="{ red: isRed }" @click="isRed = !isRed">am red</p>
+  <p :style="{color: green}" @click="toggleGreen">am green</p>
+</template>
+
+<style>
+.red{
+  color: red
+}
+</style>
