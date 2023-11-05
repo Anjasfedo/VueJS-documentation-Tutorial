89e000acd352efebfd1e056ae4b5a26604615f6e
AnjasFedo <fedoaf98@gmail.com>
2023-11-05 21:57:31 +0700

    Create onClickDoubleClick.vue

---
+++ onClickDoubleClick.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, watchEffect } from 'vue'
+
+const msg = ref('hai')
+const show = ref(false)
+
+const toggleShow = () => {
+  show.value = !show.value
+}
+</script>
+
+<template>
+<label v-if="!show" @dblclick="toggleShow">{{msg}}</label>
+<input v-if="show" type="text" v-model="msg" @change="toggleShow" @vue:mounted="({ el }) => el.focus()">
+</template>
+
+<style>
+
+</style>
